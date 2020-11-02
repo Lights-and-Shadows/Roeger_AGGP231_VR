f044e2be2659b721a8843af7ef4157dc1af7b93b
Jamie R <36210321+Lights-and-Shadows@users.noreply.github.com>
2020-11-02 10:23:16 -0500

    Various changes
    Fixing locomotion issues + reduced plane count to 3.

---
+++ Assets/XR/Scripts/InteractionHighlight.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InteractionHighlight : MonoBehaviour
+{
+    public Material mat_Hover = null;
+    public Material mat_Hightlight = null;
+
+    private Material mat_Original = null;
+    private MeshRenderer meshRenderer = null;
+    private XRGrabInteractable grabbableObj = null;
+
+    private void Awake()
+    {
+        mat_Original = GetComponent<Material>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        grabbableObj = GetComponent<XRGrabInteractable>();
+
+        grabbableObj.onActivate.AddListener(SetHighlight);
+        grabbableObj.onDeactivate.AddListener(SetOriginal);
+        grabbableObj.onHoverEnter.AddListener(SetHover);
+    }
+
+    private void OnDestroy()
+    {
+        grabbableObj.onActivate.RemoveListener(SetHighlight);
+        grabbableObj.onDeactivate.RemoveListener(SetOriginal);
+        grabbableObj.onDeactivate.RemoveListener(SetHover);
+    }
+
+    private void SetHover(XRBaseInteractor arg0)
+    {
+        meshRenderer.material = mat_Hover;
+    }
+
+    private void SetOriginal(XRBaseInteractor arg0)
+    {
+        meshRenderer.material = mat_Original;
+    }
+
+    private void SetHighlight(XRBaseInteractor arg0)
+    {
+        meshRenderer.material = mat_Hightlight;
+    }
+
+}
